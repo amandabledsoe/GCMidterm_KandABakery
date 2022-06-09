7146d576a24e1555f942ee7c5c3b022db5d1292e
amandabledsoe <amandajbledsoe@gmail.com>
2022-06-08 22:05:20 -0400

    Added Credit Card Class

---
+++ Midterm-KandABakery/CreditCard.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Midterm_KandABakery
+{
+    public class CreditCard
+    {
+        public ulong CreditCardNumber { get; set; }
+        public int ExpirationMonth { get; set; }
+        public int ExpirationYear { get; set; }
+        public int CVV { get; set; }
+
+        public CreditCard()
+        {
+
+        }
+
+        public CreditCard(ulong creditCardNumber, int expirationMonth, int expirationYear, int cVV)
+        {
+            CreditCardNumber = creditCardNumber;
+            ExpirationMonth = expirationMonth;
+            ExpirationYear = expirationYear;
+            CVV = cVV;
+        }
+    }
+}
